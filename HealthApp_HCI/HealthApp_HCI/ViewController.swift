90bb827d33ee7b711429a9e6b3a6d54dacad9631
Marty N <mdrnodado@gmail.com>
2022-04-17 17:50:04 -0700

    initialized project onto ios

---
+++ HealthApp_HCI/HealthApp_HCI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthApp_HCI
+//
+//  Created by Marty Nodado on 4/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- android/app/src/main/kotlin/com/axelsiliezar/healthapp_hci/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.axelsiliezar.healthapp_hci
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
